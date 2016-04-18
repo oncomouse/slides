dbaac84d8532dbbb2526cb9ad2e3eb3126cb8f59
onocmouse <onocmouse@gmail.com>
2016-04-18 08:37:58 -0500

    Updated remark to latest version and added a new progress-bar that uses Remark's native event system.

--- source/javascripts/remark/add-ons/progress-bar.js
+++ source/javascripts/remark/add-ons/progress-bar.js
@@ -1,24 +1,29 @@
-function attach_progress_bar() {
+function attach_progress_bar(slideshow) {
+	console.log(slideshow);
+	
+	var showSlideListener = function(current_slide){
+		if(current_slide === undefined) {
+			current_slide = slideshow.getSlides()[slideshow.getCurrentSlideIndex()];
+		}
+		$("#remark-progress-content").width(
+			(slideshow.getSlideCount() == 1) ?
+			0 : (current_slide.getSlideIndex()) / (slideshow.getSlideCount()-1) * 100 + '%'
+		);
+	};
+	
+	
 	$('.remark-slide-number').css('display', 'none');
 	$("body").append("<div id='remark-progress'></div>");
 	var progress_bar = $("#remark-progress");
-	var total_slides = parseInt($($('.remark-slide-number').get(0)).html().split(" / ")[1]);
 	
-	progress_bar.append('<div id="remark-progress-content"></div>')
-	
+	progress_bar.append('<div id="remark-progress-content"></div>');
 	$(window).on("resize", function() {
 		progress_bar.width($('.remark-visible .remark-slide-content').width());
 		progress_bar.offset({top: $(document).height() - progress_bar.offset().height, left: $('.remark-visible .remark-slide-content').offset().left});
 	});
-	$(window).on("hashchange", function(){
-		var current_slide = parseInt($('.remark-visible .remark-slide-number').html().split(" / ")[0]);
-		var progress_bar_width = (current_slide - 1) / (total_slides - 1) * 100 + "%"
-		if (total_slides == 1) {
-			progress_bar_width = 0;
-		}
-		$("#remark-progress-content").width(progress_bar_width);
-	});
 	
-	$(window).trigger("resize");
-	$(window).trigger("hashchange");
+	$(window).trigger('resize');
+	
+	slideshow.on("afterShowSlide", showSlideListener);
+	showSlideListener(undefined);
 }
