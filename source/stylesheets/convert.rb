de61fa41bfea2d36cd1f6748c20fb3899e9e0a81
onocmouse <onocmouse@gmail.com>
2016-06-29 14:08:55 -0500

    Things we do for love

--- source/stylesheets/convert.rb
+++ source/stylesheets/convert.rb
@@ -1,14 +1,19 @@
-require 'rubygems'
-require 'sass'
-require 'sass/exec'
+#require 'rubygems'
+#require 'sass'
+#require 'sass/exec'
 require 'fileutils'
+#
+#Dir.glob("**/*.sass").each do |file|
+#	
+#	outputFile = file.sub(/\.sass$/,".scss")
+#	output = Sass::Engine.new(IO.read(file)).to_tree.send("to_scss").gsub(/  /,"\t")
+#	File.open(outputFile, "w") do |fp|
+#		fp.write output
+#	end
+#	FileUtils.rm(file)
+#end
 
-Dir.glob("**/*.sass").each do |file|
-	
-	outputFile = file.sub(/\.sass$/,".scss")
-	output = Sass::Engine.new(IO.read(file)).to_tree.send("to_scss").gsub(/  /,"\t")
-	File.open(outputFile, "w") do |fp|
-		fp.write output
-	end
-	FileUtils.rm(file)
+Dir.glob("**/*.css.scss").each do |file|
+	outputFile = file.sub(/\.css\.scss$/,".scss")
+	FileUtils.mv(file, outputFile)
 end
