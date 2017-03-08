54db800045718d835b4808276551a5294aa71d31
onocmouse <onocmouse@gmail.com>
2017-03-08 10:26:47 -0600

    Added RailsAssets paths to Compass.

--- config.rb
+++ config.rb
@@ -56,6 +56,9 @@ activate :sprockets
 if defined? RailsAssets
   RailsAssets.load_paths.each do |path|
     sprockets.append_path path
+	compass_config do |config|
+		config.add_import_path path
+	end
   end
 end
 
