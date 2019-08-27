a42997dcdcf7471fde7c4c0d130d19b017e41ec4
oncomouse <oncomouse@gmail.com>
2019-08-27 14:02:05 -0500

    Formatting

--- config.rb
+++ config.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 ###
 # Compass
 ###
 
 set :markdown_engine, :kramdown
-set :markdown, :fenced_code_blocks => true,
-							 :autolink => true,
-							 :smartypants => true,
-							 :footnotes => true,
-							 :superscript => true
+set :markdown, fenced_code_blocks: true,
+               autolink: true,
+               smartypants: true,
+               footnotes: true,
+               superscript: true
 
 Haml::TempleEngine.disable_option_validator!
 
@@ -16,10 +18,10 @@ Haml::TempleEngine.disable_option_validator!
 ###
 
 configure :build do
-	set :http_prefix, "/slides"
+  set :http_prefix, '/slides'
 end
 
-page "*.html", :layout => "remark"
+page '*.html', layout: 'remark'
 
 ###
 # Helpers
@@ -28,30 +30,30 @@ page "*.html", :layout => "remark"
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
-set :build_dir, "docs"
+set :build_dir, 'docs'
 
 class String
-	def naturalized
-		scan(/[^\d\.]+|[\d\.]+/).collect { |f| f.match(/\d+(\.\d+)?/) ? f.to_f : f }
-	end
+  def naturalized
+    scan(/[^\d\.]+|[\d\.]+/).collect { |f| f.match(/\d+(\.\d+)?/) ? f.to_f : f }
+  end
 end
 
 helpers do
-	def javascript_path(file_path)
-		asset_path(:js, file_path)
-	end
-    def image_path(file_path)
-        asset_path(:images, file_path)
-    end
+  def javascript_path(file_path)
+    asset_path(:js, file_path)
+  end
 
-	def directories_with_slides
-		sitemap.resources.map{ |resource| if (!(resource.destination_path =~ /(javascripts|stylesheets|images|fonts)\//) and resource.destination_path =~ /\//) then resource.destination_path.gsub(/\/.*\.[A-Za-z]+$/,"") end }.delete_if {|x| x.nil? }.uniq
-	end
+  def image_path(file_path)
+    asset_path(:images, file_path)
+  end
 
+  def directories_with_slides
+    sitemap.resources.map { |resource| if resource.destination_path !~ %r{(javascripts|stylesheets|images|fonts)/} && resource.destination_path =~ %r{/} then resource.destination_path.gsub(%r{/.*\.[A-Za-z]+$}, '') end }.delete_if(&:nil?).uniq
+  end
 end
 
 ready do
-    ignore "remark_markdown_template.html"
+  ignore 'remark_markdown_template.html'
 end
 
 activate :sprockets
@@ -68,28 +70,28 @@ end
 # We have to proxy both kinds of files for specific reasons related
 # to building the repository.
 #
-parse_files = Dir.entries(File.join(root, "source"))
-while parse_files.length > 0
-    file = parse_files.shift
-    next if file =~ /^\./
-    next if file =~ /remark_base/
+parse_files = Dir.entries(File.join(root, 'source'))
+until parse_files.empty?
+  file = parse_files.shift
+  next if file =~ /^\./
+  next if file =~ /remark_base/
 
-	# If the file is a Markdown slide source, we proxy it to remark_markdown_template.html.haml
-	# This file will actually set up all the JavaScript and page layout things for
-	# the Markdown file, while loading the source as a Textarea, as Remark.js likes.
-    if file =~ /(\.markdown|\.md)$/
-        markdown_source = ""
-        File.open("#{Dir.pwd}/source/#{file}") do |f|
-          markdown_source = f.read
-        end
-        proxy "#{file.sub(File.extname(file), "")}", "remark_markdown_template.html", :locals => {:markdown_source => file}
+  # If the file is a Markdown slide source, we proxy it to remark_markdown_template.html.haml
+  # This file will actually set up all the JavaScript and page layout things for
+  # the Markdown file, while loading the source as a Textarea, as Remark.js likes.
+  if file =~ /(\.markdown|\.md)$/
+    markdown_source = ''
+    File.open("#{Dir.pwd}/source/#{file}") do |f|
+      markdown_source = f.read
     end
+    proxy file.sub(File.extname(file), '').to_s, 'remark_markdown_template.html', locals: { markdown_source: file }
+  end
 
-	# If the file is a directory, we proxy a directory index to /index.html.erb,
-	# which will build the index display for the various slide files we are generating.
-	if File.directory? "#{Dir.pwd}/source/#{file}" and !(file =~ /(javascripts|stylesheets|images|fonts|layouts)/) and !(file =~ /^\./)
-        proxy "#{file}/index.html", "index.html", :locals => {:directory => file}
-        parse_files += Dir.entries("#{Dir.pwd}/source/#{file}").map! { |x| if x =~ /^\./ then nil else "#{file}/#{x}" end }
-        parse_files.compact!.uniq!
-	end
+  # If the file is a directory, we proxy a directory index to /index.html.erb,
+  # which will build the index display for the various slide files we are generating.
+  next unless File.directory?("#{Dir.pwd}/source/#{file}") && file !~ /(javascripts|stylesheets|images|fonts|layouts)/ && file !~ /^\./
+
+  proxy "#{file}/index.html", 'index.html', locals: { directory: file }
+  parse_files += Dir.entries("#{Dir.pwd}/source/#{file}").map! { |x| x =~ /^\./ ? nil : "#{file}/#{x}" }
+  parse_files.compact!.uniq!
 end
