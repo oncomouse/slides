685f002a2a189fda32b2bba58f881aa3930ecfb5
oncomouse <oncomouse@gmail.com>
2019-08-29 16:04:51 -0500

    Working

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,13 @@
 
 // 1. LOAD PLUGINS
 
-var gulp = require('gulp');
-var bourbon = require('bourbon').includePaths;
-var neat = require('bourbon-neat').includePaths;
-var p = require('gulp-load-plugins')({ // This loads all the other plugins.
+const gulp = require('gulp');
+const bourbon = require('bourbon').includePaths;
+const neat = require('bourbon-neat').includePaths;
+const importer = require('node-sass-package-importer')
+const p = require('gulp-load-plugins')({ // This loads all the other plugins.
   DEBUG: false,
-  pattern: ['gulp-*', 'gulp.*', 'del', 'run-*', 'browser*', 'vinyl-*'],
+  pattern: ['gulp-*', 'gulp.*', 'del', 'vinyl-*'],
   rename: {
     'vinyl-source-stream': 'source',
     'vinyl-buffer': 'buffer'
@@ -18,39 +19,25 @@ var p = require('gulp-load-plugins')({ // This loads all the other plugins.
 
 // 2. CONFIGURATION
 
-var
-  src = 'source/', // The Middleman source folder
-  dest = '.tmp/',   // The "hot" build folder used by Middleman's external pipeline
-
-  development = p.environments.development,
-  production = p.environments.production,
-
-  css = {
-    in: src + 'stylesheets/**/*.{css,scss,sass}',
-    out: dest + 'stylesheets/',
-  },
-
-  sassOpts = {
-    imagePath: '../images',
-    includePaths: [bourbon, neat],
-    errLogToConsole: true
-  },
-
-  autoprefixerOpts = {
-  },
-
-  images = {
-    in: src + 'images/*',
-    out: dest + 'images/'
-  },
-
-  serverOpts = {
-    proxy: 'localhost:4567',
-    open: true,
-    reloadDelay: 700,
-    files: [dest + '**/*.{js,css}', src + '**/*.{html,haml,markdown}']
-  };
-
+const src = 'source/' // The Middleman source folder
+const dest = '.tmp/'   // The "hot" build folder used by Middleman's external pipeline
+const development = p.environments.development
+const production = p.environments.production
+const css = {
+  in: src + 'stylesheets/**/*.{css,scss,sass}',
+  out: dest + 'stylesheets/',
+}
+const sassOpts = {
+  imagePath: '../images',
+  includePaths: [src + 'stylesheets/'],
+  errLogToConsole: true,
+  importer: importer(),
+}
+const autoprefixerOpts = {}
+const images = {
+  in: src + 'images/*',
+  out: dest + 'images/'
+}
 // 3. WORKER TASKS
 
 // CSS Preprocessing
@@ -99,12 +86,8 @@ gulp.task('development', gulp.series('clean', gulp.parallel('css', 'images'), 's
 // This is the task that will be invoked by Middleman's exteranal pipeline when
 // running 'middleman server'
 gulp.task('default', gulp.series('development', function () {
-
-  p.browserSync.init(serverOpts);
-
   gulp.watch(css.in, gulp.series('css'));
   gulp.watch(images.in, gulp.series('images'));
-
 }));
 
 function handleError(err) {
